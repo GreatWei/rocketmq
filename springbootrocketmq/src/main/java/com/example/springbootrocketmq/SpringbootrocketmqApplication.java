a9408bdba7ed73ce5f41c8fd423ef7886847d321
GreatWei <1121412775@qq.com>
2019-01-15 00:02:34 +0800

    Signed-off-by: GreatWei <1121412775@qq.com>

---
+++ springbootrocketmq/src/main/java/com/example/springbootrocketmq/SpringbootrocketmqApplication.java
@@ -0,0 +1,14 @@
+package com.example.springbootrocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootrocketmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootrocketmqApplication.class, args);
+    }
+
+}
+
---
+++ springbootrocketmq/src/test/java/com/example/springbootrocketmq/SpringbootrocketmqApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.springbootrocketmq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootrocketmqApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
